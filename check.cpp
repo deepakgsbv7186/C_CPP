dfea290a363d64a9d0d9530a9e1010b25ee10ccd
Deepak Verma <deekgsbv@gmail.com>
2020-06-23 16:31:18 +0530

    Test input data

---
+++ check.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int ask = 9;
+    cout<<"Enter number: ";
+    cin>>ask;
+    cout<<"Hello WOrld "<<ask;
+    return 0;
+}
+git remote add origin https://github.com/deepakgsbv7186/TestPrograms.git
